2c403f84d43091946a82d9cab6b3b4d010611029
Dariell Vasquez <dariellv@yahoo.com>
2018-10-11 13:12:41 -0400

    Created google map component/integration, added styling to table data

--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -1,23 +1,25 @@
 import React, {Component} from "react";
 import { connect } from "react-redux";
-import {Sparklines, SparklinesLine} from "react-sparklines";
 import WeatherChart from "../components/WeatherChart";
+import GoogleMap from "../components/GoogleMap";
 
 class WeatherList extends Component {
     constructor(props) {
         super(props);
     }
     renderWeather(cityData) {
-        const name = cityData.city.name;
+        const {city} = cityData;
+        const {name} = city;
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressure = cityData.list.map(weather => weather.main.pressure);
         const humidity = cityData.list.map(weather => weather.main.humidity);
+        const {lon, lat} = city.coord;
         return (
             <tr key={name}>
-                <td>{name}</td>
-                <WeatherChart data={temps} color="blue" height={100} />
-                <WeatherChart data={pressure} color="red" height={130} />
-                <WeatherChart data={humidity} color="green" height={130}/>
+                <td><GoogleMap lon={lon} lat={lat}/></td>
+                <WeatherChart  data={temps} color="blue" units="K" />
+                <WeatherChart  data={pressure} color="red" units="hPa" />
+                <WeatherChart data={humidity} color="green" units="%" />
             </tr>
         )
     }
@@ -27,9 +29,9 @@ class WeatherList extends Component {
             <thead>
                 <tr>
                     <th>City</th>
-                    <th>Temperature</th>
-                    <th>Pressure</th>
-                    <th>Humidity</th>
+                    <th>Temperature (K)</th>
+                    <th>Pressure (hPa)</th>
+                    <th>Humidity (%)</th>
                 </tr>
             </thead>
             <tbody>
